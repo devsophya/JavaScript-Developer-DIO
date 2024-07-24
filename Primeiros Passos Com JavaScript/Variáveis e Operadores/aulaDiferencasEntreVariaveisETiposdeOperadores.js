99784ce115927694f7b9c27091afb1b31e0ee501
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-24 12:38:42 -0300

    aulaDiferencasEntreVariaveisETiposdeOperadores.js - concluida com êxito.
    aulaDiferencasEntreVariaveisETiposdeOperadores.js

---
+++ Primeiros Passos Com JavaScript/Variáveis e Operadores/aulaDiferencasEntreVariaveisETiposdeOperadores.js
@@ -0,0 +1,30 @@
+let variavel = 10;
+
+console.log(variavel);
+
+variavel = 20;
+
+console.log(variavel); 
+
+
+/* Começando a aplicar operadores matemáticos */
+
+/*adição*/
+let variavel = 10 + 10;
+ 
+console.log(variavel);
+
+/* subtração*/
+let variavel = 10 - 10;
+
+console.log(variavel);
+
+/*multiplicação*/ 
+let variavel = 10 * 10;
+
+console.log(variavel);
+
+/* divisão*/
+let variavel = 10 / 10;
+
+console.log(variavel);
