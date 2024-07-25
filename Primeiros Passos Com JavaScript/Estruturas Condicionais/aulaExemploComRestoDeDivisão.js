5df48efbbd21254d48d693f67300a910c3da3bbb
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-25 12:40:45 -0300

    aulaExemploComRestoDeDivisão - concluída com êxito
    aulaExemploComRestoDeDivisão.js

---
+++ Primeiros Passos Com JavaScript/Estruturas Condicionais/aulaExemploComRestoDeDivisão.js
@@ -0,0 +1,11 @@
+/* anotações da aula
+exemplo: se o número for múltiplo de 2 não sobra resto na divisão (número par), se sobrar resto ele ele não é múltiplo de 2 (número ímpar)
+
+*/
+
+
+const numero = 10;
+
+const NumeroPar = (numero % 2) === 0;
+
+console.log(numeroPar);
