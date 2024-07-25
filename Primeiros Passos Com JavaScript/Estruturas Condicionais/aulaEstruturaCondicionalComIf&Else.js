9e5009d8b9d002bfea745617f3355d1972c2475e
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-25 13:34:04 -0300

    aulaEstruturaCondicionalComIf&Else - concluída com êxito.
    aulaEstruturaCondicionalComIf&Else.js

---
+++ Primeiros Passos Com JavaScript/Estruturas Condicionais/aulaEstruturaCondicionalComIf&Else.js
@@ -0,0 +1,13 @@
+/* anotações da aula 
+! - negação
+
+*/
+const numero = 15;
+
+const numeroDivisivelPor5 = (numero % 5) === 0;
+
+if (numeroDivisivelPor5) {
+     console.log('Sim')
+} else {
+    console.log('Não')
+}
