c0857af1e392a292750e34d384ec0564e7248271
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-25 16:17:53 -0300

    aulaRevisãoEDicasDeBoasPráticas - concluída com êxito.
    aulaRevisãoEDicasDeBoasPráticas.js

---
+++ Primeiros Passos Com JavaScript/Estruturas Condicionais/aulaRevisãoEDicasDeBoasPráticas.js
@@ -0,0 +1,15 @@
+/* anotações da aula
+"is" significa "é"
+*/
+
+const numero = 5;
+const isNumeroPar = (numero % 2) === 0;
+
+
+if(isNumeroPar){
+    console.log('O número é par');
+} else {
+    console.log('O número é impar');
+}
+
+ 
