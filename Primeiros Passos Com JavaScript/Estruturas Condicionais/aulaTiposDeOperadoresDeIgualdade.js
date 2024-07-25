dac986e961080abe41aa8a91cd203a29a5ac682c
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-25 12:48:58 -0300

    aulaTiposDeOperadoresDeIgualdade - concluída com êxito.
    aulaTiposDeOperadoresDeIgualdade.js

---
+++ Primeiros Passos Com JavaScript/Estruturas Condicionais/aulaTiposDeOperadoresDeIgualdade.js
@@ -0,0 +1,10 @@
+/*anotações da aula 
+"=" atribuição 
+"==" igualdade 
+"===" o que é mais usado para igualdade 
+
+utilizando os "==" ele ignora o tipo da variável, ou seja, compara números com texto, conversão implicita.
+
+utilizando os "===" ele não faz conversão implicita, não compara números e texto .
+*/
+
