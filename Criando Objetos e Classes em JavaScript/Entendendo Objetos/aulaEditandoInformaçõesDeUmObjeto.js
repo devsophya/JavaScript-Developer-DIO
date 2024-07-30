1da653a5475cc4eb5e1d8b7a5ce5a97df63d2a29
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-30 12:29:29 -0300

    aulaEditandoInformaçõesDeUmObjeto - concluída com êxito.
    aulaEditandoInformaçõesDeUmObjeto - concluída com êxito.

---
+++ Criando Objetos e Classes em JavaScript/Entendendo Objetos/aulaEditandoInformaçõesDeUmObjeto.js
@@ -0,0 +1,34 @@
+/* objeto no js é sempre uma coleção dinâmica de chave e valor
+
+*/
+
+const sophya = {
+    nome: 'Sophya Damiazo',
+    idade: 19
+};
+
+sophya.altura = 1.74;
+
+delete sophya.nome;
+
+console.log(sophya);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
