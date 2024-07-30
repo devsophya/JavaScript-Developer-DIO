d3a67330840e3430d3f5e30ab06424c06885cfb4
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-29 21:46:16 -0300

    aulaExercicio3InvocandoFunçãoDentroDeOutraFunção - concluída com êxito.
    aulaExercicio3InvocandoFunçãoDentroDeOutraFunção - concluída com êxito.

---
+++ Conhecendo Funções Javascript/Praticando com Funções/aulaExercicio3InvocandoFunçãoDentroDeOutraFunção.js
@@ -0,0 +1,28 @@
+function escrevaMeuNome(name){
+    return 'Meu nome é ' + name;
+}
+
+
+function verificarIdade(idade){
+    if (idade >= 18){
+        console.log(escrevaMeuNome('Sophya') + ' É maior de idade');
+    } else {
+        console.log('Menor de idade');
+    }
+}
+
+
+verificarIdade(19);
+
+
+
+
+
+
+
+
+
+
+
+
+
