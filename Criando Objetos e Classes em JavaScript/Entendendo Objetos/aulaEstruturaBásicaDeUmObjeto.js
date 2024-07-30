88205ca0deb184d3f4eb0f470fec1714e47396fd
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-30 12:23:30 -0300

    aulaEstruturaBásicaDeUmObjeto - concluída com êxito.
    aulaEstruturaBásicaDeUmObjeto - concluída com êxito.

---
+++ Criando Objetos e Classes em JavaScript/Entendendo Objetos/aulaEstruturaBásicaDeUmObjeto.js
@@ -0,0 +1,16 @@
+/* objetos são coleções de dados, agrupam valores 
+e funcionam através de chave e valor
+
+*/
+
+const sophya = {
+    nome: 'Sophya Damiazo',
+    idade: 19
+};
+
+console.log(sophya.nome);
+console.log(sophya.idade);
+console.log(sophya);
+
+
+
