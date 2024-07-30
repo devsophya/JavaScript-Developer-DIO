e6ff9fbd36ea967327ac906557b786bcbe4f187d
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-29 21:35:23 -0300

    aulaExercicio2FunçãoVerificarIdade - concluída com êxito.
    aulaExercicio2FunçãoVerificarIdade - concluída com êxito.

---
+++ Conhecendo Funções Javascript/Praticando com Funções/aulaExercicio2FunçãoVerificarIdade.js
@@ -0,0 +1,13 @@
+
+
+
+function verificarIdade(idade){
+    if (idade >= 18){
+        console.log('Maior de idade');
+    } else {
+        console.log('Menor de idade');
+    }
+}
+
+
+verificarIdade(19);
