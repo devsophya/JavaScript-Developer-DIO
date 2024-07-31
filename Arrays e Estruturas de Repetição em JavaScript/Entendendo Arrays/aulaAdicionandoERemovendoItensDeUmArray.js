cd99f5f52871d7f8c905fb91073c76b92aedeb04
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-31 11:57:53 -0300

    aulaAdicionandoERemovendoItensDeUmArray - concluída com êxito.
    aulaAdicionandoERemovendoItensDeUmArray - concluída com êxito.

---
+++ Arrays e Estruturas de Repetição em JavaScript/Entendendo Arrays/aulaAdicionandoERemovendoItensDeUmArray.js
@@ -0,0 +1,34 @@
+
+
+const alunos = ['Sophya', 'Gustavo', 'Marcello'];
+
+alunos.push('Tania');
+
+alunos[4] = 'Forms';
+
+console.log(alunos);
+ 
+/* tipos diferentes de variaveis */
+
+const alunos = [];
+
+alunos.push ('teste');
+
+alunos.push(10);
+
+console.log(alunos);
+
+/* removendo da lista e imprimindo direto*/
+
+const alunos = ['1', '2', '3'];
+
+console.log(alunos);
+
+console.log(alunos.pop()); // pega o ultimo da fila
+
+console.log(alunos.shift()); // pega o primeiro da fila
+
+console.log(alunos);
+
+
+
