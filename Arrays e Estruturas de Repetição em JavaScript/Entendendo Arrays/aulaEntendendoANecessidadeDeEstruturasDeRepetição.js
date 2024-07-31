0ccada1d28438fd0cf483e65cd377c0900bbd669
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-31 12:05:00 -0300

    aulaEntendendoANecessidadeDeEstruturasDeRepetição - concluída com êxito.
    aulaEntendendoANecessidadeDeEstruturasDeRepetição - concluída com êxito.

---
+++ Arrays e Estruturas de Repetição em JavaScript/Entendendo Arrays/aulaEntendendoANecessidadeDeEstruturasDeRepetição.js
@@ -0,0 +1,18 @@
+
+const notas =[];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+const soma = notas[0] + notas[1] + notas[2] +notas[3] + notas[4];
+
+console.log(soma / 5);
+
+
+
+
+
