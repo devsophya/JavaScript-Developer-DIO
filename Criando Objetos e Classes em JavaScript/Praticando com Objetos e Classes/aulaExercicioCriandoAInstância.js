924fa083b02ba666d4dfa2a28cb8f8e0036859e4
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-30 21:32:00 -0300

    aulaExercicioCriandoAInstância - concluída com êxito.
    aulaExercicioCriandoAInstância - concluída com êxito.

---
+++ Criando Objetos e Classes em JavaScript/Praticando com Objetos e Classes/aulaExercicioCriandoAInstância.js
@@ -0,0 +1,24 @@
+/* exercício 
+Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por KM rodado.
+Crie um método que dado a quantidade de quilomentros e preço do combustivel nos dê o valor
+gasto em reais para realizar este percurso.
+*/
+
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+constructor (marca, cor, gastoMedioPorKm) {
+    this.marca = marca;
+    this.cor = cor;
+    this.gastoMedioPorKm = gastoMedioPorKm;
+}
+
+}
+
+const uno = new Carro('Fiat', 'Prata', 1/12);
+console.log(uno);
+
+
