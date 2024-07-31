a488a4ee09e55efe50e3196a45a626f45e1e3938
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-31 10:52:49 -0300

    aulaCriandoOMetodoCalcularImc - concluída com êxito.
    aulaCriandoOMetodoCalcularImc - concluída com êxito.

---
+++ Criando Objetos e Classes em JavaScript/Praticando com Objetos e Classes/aulaCriandoOMetodoCalcularImc.js
@@ -0,0 +1,29 @@
+/* exercicio 2
+
+Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC= peso / (latura * altura));
+Instancie uma pessoa Gustavo que tenha 70kg de peso e 1,75 e peça ao Gustavo para dizer o valor 
+do seu IMC;
+
+*/
+
+class Pessoa{
+    nome;
+    peso;
+    altura;
+
+   constructor (nome,peso,altura){
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+   }
+    
+calcularImc(){
+   return this.peso /(this.altura * this.altura);
+}
+
+
+}
+
+
