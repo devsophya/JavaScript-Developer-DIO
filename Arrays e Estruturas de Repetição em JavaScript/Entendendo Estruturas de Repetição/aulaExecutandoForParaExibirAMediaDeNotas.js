def2ef1092a3ba5336a2553d11f5ccc5c00e0a1f
Sophya <148731999+devsophya@users.noreply.github.com>
2024-07-31 12:26:53 -0300

    aulaExecutandoForParaExibirAMediaDeNotas - concluída com êxito.
    aulaExecutandoForParaExibirAMediaDeNotas - concluída com êxito.

---
+++ Arrays e Estruturas de Repetição em JavaScript/Entendendo Estruturas de Repetição/aulaExecutandoForParaExibirAMediaDeNotas.js
@@ -0,0 +1,19 @@
+
+const notas =[];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(6);
+notas.push(5);
+notas.push(8);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+}
+
+const media = soma / notas.length;
+console.log(media);
