da3bd523e149431e1d5d83c4a75426f532cb291a
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-09 10:57:00 -0300

    aulaMenorNumeroImpar - concluída com êxito.
    aulaMenorNumeroImpar - concluída com êxito.

---
+++ Desafios de Código JavaScript/aulaDesafio2MaiorParEMenorÍmpar/aulaMenorNumeroImpar.js
@@ -0,0 +1,41 @@
+
+/* Faça um programa que receba N (quantidade de números) e seus respectivos valores.
+Imprima o maior número par e o menor número ímpar.
+
+Ex: Entrada :
+        5
+        3
+        4
+        1
+        10
+        8
+
+    Saída:
+        Maior número par: 10
+        Menor número impar: 1
+*/
+
+const { gets, print } = require('./mainEx2');
+
+const n = gets();
+let maiorNumeroPar = 0;
+let menorNumeroImpar = 0;
+
+for (let i = 0; i < n; i++) {
+    const numero = gets();
+    if (numero % 2 === 0) {
+        if (numero > maiorNumeroPar) {
+            maiorNumeroPar = numero;
+        }
+    } else {
+        if (menorNumeroImpar === 0) {
+            menorNumeroImpar = numero;
+        } else if(numero< menorNumeroImpar){
+            menorNumeroImpar = numero;
+        }
+    }
+
+}
+
+print('Maior número par: ' + maiorNumeroPar);
+print('Menor número ímpar: ' + menorNumeroImpar);
