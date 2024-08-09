4daaf3d542a7b8839980c8463646e1cd68037846
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-09 13:00:23 -0300

    aulaValorATransferir - concluída com êxito.
    aulaValorATransferir - concluída com êxito.

---
+++ Desafios de Código JavaScript/aulaDesafio3SalárioATransferir/aulaValorATransferir.js
@@ -0,0 +1,47 @@
+/* Faça um programa que calcule e imprima o salário a ser transferido
+para um funcionário.
+Para realizar o cálculo receba o valor bruto do salário e o adicional dos beneficios.
+O salário a ser transferido é calculado da seguinte maneira:
+
+valor bruto do salário - percentual de imposo mediante a faixa salarial + adição 
+dos benefícios.
+
+Para calcular o percentual de imposto segue as aliquotas:
+
+De R$ 0.00 a 1100.00 = 5.00%
+De 1100.01 a 2500.00 = 10.00%
+Maior que R$ 2500.00 = 15.00%
+
+Ex: Entrada: 
+        2000
+        250
+
+    Saída:
+        2050.00
+*/
+
+const { gets, print } = require('./mainDesafio3');
+
+const salarioBruto = gets();
+const beneficios = gets();
+
+function calcularPorcentagem(valor ,percentual){
+  return valor * (percentual/100);
+}
+
+function pegarPercentualImpostoComBaseNoSalario(salario){
+
+    if (salario >= 0 && salario <= 1100) {
+        return 5;
+    }else if (salario >= 1100.01 && salario <= 2500){
+        return 10;
+    } else {
+        return 15;
+    }
+}
+
+const valorATransferir = salarioBruto - (calcularPorcentagem(salarioBruto,
+pegarPercentualImpostoComBaseNoSalario(salarioBruto))) + beneficios;
+
+print(valorATransferir);
+
