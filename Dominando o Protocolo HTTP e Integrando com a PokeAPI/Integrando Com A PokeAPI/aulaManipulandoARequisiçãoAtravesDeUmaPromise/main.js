50d2ff05db19f67b5dff9b54853d665aef735d61
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-27 08:01:16 -0300

    aulaManipulandoARequisiçãoAtravesDeUmaPromise - concluída com êxito.
    aulaManipulandoARequisiçãoAtravesDeUmaPromise

---
+++ Dominando o Protocolo HTTP e Integrando com a PokeAPI/Integrando Com A PokeAPI/aulaManipulandoARequisiçãoAtravesDeUmaPromise/main.js
@@ -0,0 +1,13 @@
+
+const offset = 0
+const limit = 10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+
+
+
+
