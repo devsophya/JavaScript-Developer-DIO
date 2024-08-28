8da053c5116159293764d0837fa2dfa0479d519e
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-28 12:33:53 -0300

    aulaManipulandoMultiplasRequisiçõesEmParalelo - concluída com êxito.
    aulaManipulandoMultiplasRequisiçõesEmParalelo - concluída com êxito.

---
+++ Dominando o Protocolo HTTP e Integrando com a PokeAPI/Integrando Com A PokeAPI/aulaManipulandoMultiplasRequisiçõesEmParalelo/main.js
@@ -0,0 +1,31 @@
+
+function convertPokemonTypeToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`) 
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+             <span class="number">#${pokemon.order}</span>
+                <span class="name">${pokemon.name}</span>
+                    
+                <div class="detail">
+                        <ol class="types">
+                         ${convertPokemonTypeToLi(pokemon.types).join('')}
+                        </ol>
+                        <img src="${pokemon.sprites.other.dream_world.front_default}"
+                            alt="${pokemon.name}">
+
+                </div>
+        </li>
+                
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+     const newHtml = pokemons.map(convertPokemonToLi).join('')
+     pokemonList.innerHTML = newHtml
+})
+
