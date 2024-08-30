8628d6f73d181a8f1e7cd8b969b0fc0f5936b23a
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-30 12:05:58 -0300

    aulaFunctions - concluída com êxito.
    aulaFunctions - concluída com êxito.

---
+++ Funções e Manipulação de Listas com JavaScript/Funções/aulaFunctions/index.js
@@ -0,0 +1,29 @@
+
+//First Class Functions
+//High Order Functions
+
+/*
+function falarMeuNome() {
+    console.log('Meu nome é Sophya')
+}
+
+function falarMeuNomeCompleto(falarMeuNome){
+    falarMeuNome()
+    console.log('Damiazo')
+    return falarMeuNome
+}
+
+falarMeuNomeCompleto(falarMeuNome)()
+*/
+
+console.log('bhfasdk')
+
+function nomeDaFuncao() {
+    console.log('nomeDaFuncao')
+}
+
+const nomeDeOutraFuncao = function(){
+    console.log('nomeDeOutraFuncao')
+}
+nomeDaFuncao()
+nomeDeOutraFuncao()
