ec8d4a56afe969f450e111921c96363e00ee83a8
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-30 09:30:55 -0300

    aulaTiposDeDados - concluída com êxito.
    aulaTiposDeDados - concluída com êxito.

---
+++ Aprendendo Variáveis, Escopo e Tipos de Dados no JavaScript/Tipos de Dados/aulaTiposDeDados/tipos-dados.js
@@ -0,0 +1,30 @@
+/* 
+    boolean 
+    null
+    undefined
+    number
+    string
+    synbol
+
+*/ 
+
+//  object
+
+
+new Boolean(true) //(wrappers) 
+
+console.log(10 == '10') // ele faz a equiparação dos tipos para depois comparar , considera os tipos  
+console.log(10 === '10') // desconsidera os tipos e compara 
+
+// Boolean 
+
+const nome = '';
+
+if (nome) {
+    console.log('preenchido')
+}
+// "!" usado para negação 
+
+// Null & Undefined:
+//null significa que a variavel existe mas tem ausência de valor 
+// undefined é ausência de definição
