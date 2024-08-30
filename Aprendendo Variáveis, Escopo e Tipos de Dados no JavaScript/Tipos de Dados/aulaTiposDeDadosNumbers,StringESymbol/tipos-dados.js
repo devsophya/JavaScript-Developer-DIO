a36b428c5ef21c52da252a4b25b7e47e10f3c14a
Sophya <148731999+devsophya@users.noreply.github.com>
2024-08-30 09:53:32 -0300

    aulaTiposDeDadosNumbers,StringESymbol - concluída com êxito.
    aulaTiposDeDadosNumbers,StringESymbol - concluída com êxito.

---
+++ Aprendendo Variáveis, Escopo e Tipos de Dados no JavaScript/Tipos de Dados/aulaTiposDeDadosNumbers,StringESymbol/tipos-dados.js
@@ -0,0 +1,19 @@
+/*
+  number
+    string
+    symbol
+*/
+
+// +Infinity -Infinity e  NaN
+// Double Precision 64-bit binary formt IEEE 754
+
+//String 
+"texto"
+'texto' // declarar 
+`texto` // concatenar
+
+//Symbol
+const x = Symbol('10');
+const y = x;
+
+console.log(x===y);
